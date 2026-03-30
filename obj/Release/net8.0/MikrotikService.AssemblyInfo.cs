bf34b675192e1cebce4bcff2e99f602fc005a2bd
TATA Ronaldo <tronaldowiysanyuy@gmail.com>
2026-03-30 20:57:29 +0100

    Fix: Replace LoadList<dynamic> with raw ExecuteList for MAC binding to bypass TikEntityAttribute error

--- obj/Release/net8.0/MikrotikService.AssemblyInfo.cs
+++ obj/Release/net8.0/MikrotikService.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MikrotikService")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+655621fa7d6b0aa17b9f800b970e5922be579b7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MikrotikService")]
